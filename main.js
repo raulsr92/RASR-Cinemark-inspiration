a2c8d30b8d2cbdbb406fade85a44b10bf739a11d
Raul Sanchez R. <raul.sr92@gmail.com>
2024-01-26 17:08:55 -0500

    Captura de elementos en constantes JS

---
+++ main.js
@@ -0,0 +1,8 @@
+/* Interacción de la ventana emergente */
+
+const botonCine = document.querySelector(".btn-header3")
+console.log(botonCine)
+
+const ventanaEmergente = document.querySelector(".componente-seleccion")
+console.log(ventanaEmergente)
+
