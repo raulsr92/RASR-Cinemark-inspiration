a27eab96a3211fbe4af4b11d0e716d0c8b559d8e
Raul Sanchez R. <raul.sr92@gmail.com>
2024-01-26 18:04:36 -0500

    Elaboración del evento click y la función que se desencadena

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ console.log(botonCine)
 const ventanaEmergente = document.querySelector(".componente-seleccion")
 console.log(ventanaEmergente)
 
+
+botonCine.addEventListener("click", apareceDesapareceVentana)
+
+function apareceDesapareceVentana() {
+    ventanaEmergente.classList.toggle("inactive")
+}
+
+
+
+
+
+/*
+function apareceDesapareceVentana() {
+    ventanaEmergente.classList.toggle("inactive") 
+}
+
+*/
